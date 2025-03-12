f426833c812bfa40aefbaee607c7840d55a3b6c6
filipbacic <filipbacic@gmail.com>
2025-03-12 02:04:03 +0100

    add book path as cli input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_chars, sort_dict_by_num
+import sys
 
 def get_book_text(path):
     file_contents = ""
@@ -7,7 +8,11 @@ def get_book_text(path):
     return file_contents
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     word_count = get_num_words(book_text)
     num_chars = get_num_chars(book_text)
